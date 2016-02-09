9e81f353426bca55d9d7ef72c114cf705df95582
dwang5462 <dwang5462@yahoo.com>
2016-02-08 23:10:19 -0800

    got it to compile

--- minHeap.cpp
+++ minHeap.cpp
@@ -5,16 +5,11 @@
 #include <vector>
 #include <string>
 
-minHeap::minHeap() {
-	heap = new vector<Node*>;
-}
-
 minHeap::minHeap(std::string file) {
-	heap = new std::vector<Node*>;
 	heapify(file);
 }
-Node* getMin(){
-	Node * min = heap.get(1);
+Node* minHeap::getMin(){
+	Node * min = heap.at(1);
 	heap.at(1) = heap.back();
 	heap.pop_back();
 	maintainInvariant();
@@ -23,24 +18,25 @@ Node* getMin(){
 
 void minHeap::heapify(std::string file){
   //this adds all the letters from a file into a vector with nodes of the letter and frequency
-  countFreq freqTable=new freqTable();
-  freqTable.addLetters(file);
+  countFreq * freqTable = new countFreq();
+  freqTable->addLetters(file);
     for(char i=97; i<=123; i++){
-      if(freqTable.getValue(i)!=0){      
-	Node p=new Node(i, freqTable.getValue(i));
-	heap.emplace_back(p);
-      }}
-    if(freqTable.getValue(46)!=0){
-      Node p=new Node(46, freqTable.getValue(46));
-      heap.emplace_back(p);}
+      if(freqTable->getValue(i)!=0){      
+	     Node * p=new Node(i, freqTable->getValue(i));
+	     heap.push_back(p);
+      }
+    }
+    if(freqTable->getValue(46)!=0){
+      Node * p=new Node(46, freqTable->getValue(46));
+      heap.push_back(p);}
 
-    if(freqTable.getValue(32)!=0){
-      Node p=new Node(32, freqTable.getValue(32));
-      heap.emplace_back(p);}
+    if(freqTable->getValue(32)!=0){
+      Node * p=new Node(32, freqTable->getValue(32));
+      heap.push_back(p);}
 
-    if(freqTable.getValue(10)!=0){
-      Node p=new Node(10, freqTable.getValue(10));
-      heap.emplace_back(p);}
+    if(freqTable->getValue(10)!=0){
+      Node * p=new Node(10, freqTable->getValue(10));
+      heap.push_back(p);}
     //now I need to do the heapifying
 
 
@@ -54,7 +50,7 @@ void minHeap::maintainInvariant() {
 
 }
 
-Node minHeap::connect(Node* min1, Node* min2) {
+Node * minHeap::connect(Node* min1, Node* min2) {
 	int newFreq = min1->getFrequency() + min2->getFrequency();
 	Node * newParent = new Node(newFreq);
 	newParent->setLchild(min1);
--- minHeap.h
+++ minHeap.h
@@ -8,12 +8,11 @@ using namespace std;
 class minHeap{
 
  public:
-  minHeap();
   minHeap(std::string file);
   Node * getMin();
   void heapify(std::string file);
   void maintainInvariant();
-  void insert();
+  void insert(Node * toInsert);
   Node * connect(Node * min1, Node * min2); //returns the new parent node of the two mins- but only one node so I think it also doesn't need a *
   Node* run();
 

9bf6305c77e118db23b46099f6aa4ce23eb02e39
Kelly Bielaski <kellybielaski@umail.ucsb.edu>
2016-02-08 23:03:44 -0800

    redid maintainInvariant

--- minHeap.cpp
+++ minHeap.cpp
@@ -6,11 +6,10 @@
 #include <string>
 
 minHeap::minHeap() {
-	heap = new vector<Node*>;
+  //heap;
 }
 
 minHeap::minHeap(std::string file) {
-	heap = new std::vector<Node*>;
 	heapify(file);
 }
 Node* getMin(){
@@ -20,31 +19,60 @@ Node* getMin(){
 	maintainInvariant();
 	return min;
 }
+void minHeap::maintainInvariant(){
+
+  for(int i=heap.size; i<0;i--){
+    p=i/2;
+    while(heap[p*2]->getFrequency()<heap[p]->getFrequency()||
+	  heap[p*2+1]->getFrequency()<heap[p]->getFrequency()){
+      if(heap[p*2]->getFrequency()<heap[p]->getFrequency()&&
+	 heap[p*2+1]->getFrequency()<heap[p]->getFrequency()){
+	if(heap[p*2]->getFrequency()<heap[p*2+1]->getFrequency()){
+	    heap[p*2].swap(heap[p]);
+	    p=p*2;
+	  }
+	else{
+	  heap[p*2+1].swap(heap[p]);
+	  p=p*2+1;
+	}
+      }
+      if(heap[p*2]->getFrequency()<heap[p]->getFrequency()){
+	heap[p*2].swap(heap[p]);
+	p=p*2;
+    }
+      else{
+	heap[p*2+1].swap(heap[p]);
+	p=p*2+1;
+      }
+  }
+}
 
 void minHeap::heapify(std::string file){
   //this adds all the letters from a file into a vector with nodes of the letter and frequency
   countFreq freqTable=new freqTable();
   freqTable.addLetters(file);
+  Node* zero=new Node('?', -100);
+  heap[0]=zero;
     for(char i=97; i<=123; i++){
       if(freqTable.getValue(i)!=0){      
-	Node p=new Node(i, freqTable.getValue(i));
-	heap.emplace_back(p);
+	Node* p=new Node(i, freqTable.getValue(i));
+	heap.push_back(p);
       }}
     if(freqTable.getValue(46)!=0){
-      Node p=new Node(46, freqTable.getValue(46));
-      heap.emplace_back(p);}
+      Node* p=new Node(46, freqTable.getValue(46));
+      heap.push_back(p);}
 
     if(freqTable.getValue(32)!=0){
-      Node p=new Node(32, freqTable.getValue(32));
-      heap.emplace_back(p);}
+      Node* p=new Node(32, freqTable.getValue(32));
+      heap.push_back(p);}
 
     if(freqTable.getValue(10)!=0){
-      Node p=new Node(10, freqTable.getValue(10));
-      heap.emplace_back(p);}
+      Node* p=new Node(10, freqTable.getValue(10));
+      heap.push_back(p);}
     //now I need to do the heapifying
-
-
 }
+
+
 void minHeap::insert(Node * toInsert){
 	heap.push_back(toInsert);
 	maintainInvariant();
